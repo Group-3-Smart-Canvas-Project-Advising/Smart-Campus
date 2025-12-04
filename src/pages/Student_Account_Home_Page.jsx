d3b813e0c21be5a5e12251253227e386db147565
Brandon-Conner <brancon@okstate.edu>
2025-12-04 11:01:35 -0600

    Update Student_Account_Home_Page.jsx
    Accidentally removed an import when merging, fixed now

--- src/pages/Student_Account_Home_Page.jsx
+++ src/pages/Student_Account_Home_Page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 import AI_Prompt_Field from "../components/AI_Prompt_Field.jsx";
 import Hamburger_Menu from "../components/Hamburger_Menu.jsx";
-
+import {Link} from "react-router-dom";
 
 
 const Student_Account_Home_Page = () =>
